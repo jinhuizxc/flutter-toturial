e96574cb7ec200f58156c717c30b0b4cb8cca41c
jinhuizxc <1004260403@qq.com>
2019-03-16 13:56:58 +0800

    ListView组件的使用

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,16 @@ class MyApp extends StatelessWidget{
       title: 'TextWidget',
       home: Scaffold(
         appBar: AppBar(
-          title: Text("TextWidget"),
+          title: Text("ListViewWidget"),
         ),
-        body: Center(
-        child: Container(
-          child: new Image.network(
-            'https://img4.mukewang.com/szimg/5c7e6835087ef3d806000338.jpg',
-            scale: 2.0,
-            repeat: ImageRepeat.repeat,
-          ),
-          width: 400.0,
-          height: 300.0,
-          color: Colors.lightBlue,
-        )
-        )
+        body: new ListView(
+          children: <Widget>[
+            new Image.network('https://img4.mukewang.com/szimg/5c18d2d8000141c506000338.jpg'),
+            new Image.network('https://img4.mukewang.com/szimg/5c7e6835087ef3d806000338.jpg'),
+            new Image.network('https://img2.mukewang.com/szimg/5c3ef588088403df06000338.jpg'),
+            new Image.network('https://img3.mukewang.com/szimg/5b3082da0001d7e905400300.jpg'),
+          ],
+        ),
       )
     );
 
